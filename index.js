97e6c0c91f9340918115fd6fae7bc23497992d89
Pranav <pranavpk404@gmail.com>
2021-11-01 16:55:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,10 +51,7 @@ async function getRepos(username) {
     const resp = await fetch(API_URL + username + "/repos");
     const respData = await resp.json();
     console.log(respData)
-    if (respData.html_url === undefined) {
-      alert("No Repos");
-    } else {
-      addReposToCard(respData);
+    addReposToCard(respData);
     }
   } catch (error) {
     alert("Some error occured");
